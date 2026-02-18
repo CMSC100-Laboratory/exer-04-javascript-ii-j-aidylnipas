0c03347a3400d4895d4da348ddc4088f28f8313e
j-aidylnipas <jtnipas@up.edu.ph>
2026-02-19 01:53:02 +0800

    Added Save File & Import Syntax

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import { appendFileSync } from 'fs'
 import {v4 as uuidv4} from 'uuid'
-import {validator} from 'validator'
+import validator from 'validator'
 
 // Juzhel Aidyl T. Nipas
 // CMSC 100 - U3L
@@ -26,32 +27,48 @@ function generateUniqueID(fname, lname) {
 }
 
 function addAccount(acc) {
-
+    
     let account = "" // account placeholder
 
-    if (acc[0] != null && typeof(acc[0]) == "string") { // if first name is present and a string
+    if (acc[0] != null && typeof(acc[0]) == "string" && acc[0].trim() !== "") { // if first name is present and a string
         account += acc[0] // add first name to the placeholder
         account += ","
+    } else {
+        return false
     }
 
-    if (acc[1] != null && typeof(acc[1]) == "string") { // if last name is present and a string
+    if (acc[1] != null && typeof(acc[1]) == "string" && acc[1].trim() !== "") { // if last name is present and a string
         account += acc[1] // add last name to the placeholder
         account += ","
+    } else {
+        return false
     }
     
-    if (acc[2] != null && typeof(acc[2]) == "string") { // if email is present and a string
-        if (validator.isEmail(acc[2]) == true) { // if given email is in valid format
-            account += acc[2] // add email to the placeholder
-            account += ","
-        }
+    if (acc[2] != null && typeof(acc[2]) == "string" && (validator.isEmail(acc[2])) && acc[2].trim() !== "") { // if email is present and a string
+        account += acc[2] // add email to the placeholder
+        account += ","        
+    } else {
+        return false
     }
 
-    if (acc[3] != null && acc[3] >= 18) { // if age is present and at least 18
+    if (typeof(acc[3]) === "number" && acc[3] >= 18) { // if age is present and at least 18
         account += acc[3] // add age to the placeholder
         account += ","
+    } else {
+        return false
     }
 
-    account += generateUniqueID(acc[0], acc[1]) // add the unique ID to the placeholder
+    const uniqueID = generateUniqueID(acc[0], acc[1]) // add the unique ID to the placeholder
+    account += uniqueID
 
+    account += "\n" // for proper file formatting
 
+    try {
+        appendFileSync('users.txt', account) // write data to file
+        return true
+    } catch (error) {
+        return false // if file writing fails
+    }
 }
+
+export { generateUniqueID, addAccount }
--- test.js
+++ test.js
@@ -3,4 +3,14 @@
 // February 18, 2026
 // Exercise 4
 
-// import ind from './index.js'
+import { generateUniqueID, addAccount } from './index.js'
+
+
+const validAcc = ["Alan", "Turing", "aturing@w3c.com", 58]
+console.log(generateUniqueID("Alan", "Turing"))
+console.log(addAccount(validAcc))
+
+
+const invalidAcc = ["Tim", "Kid", "tim@kid.com", 15]
+console.log(generateUniqueID("TIM", "KID"))
+console.log(addAccount(invalidAcc))
